ab2d13c5d09fc708487331f28e1fc538e35e3eab
Martin Ström <zicho.calypso@gmail.com>
2021-12-04 12:06:52 +0100

    added cookie typings, removed debug logging

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts" context="module">
 	export async function load({ session, page }) {
-
-		console.dir("Layout: " + session)
-
         if (!session) {
 			if (page.path != '/')
 				return {
