6f8a09dd9d4c357dc126ef660a7b749c13f9ad7a
Martin Ström <zicho.calypso@gmail.com>
2021-12-16 15:57:52 +0100

    deleted unused endpoint file

--- src/routes/api/user/profile.ts
+++
@@ -1,9 +0,0 @@
-import { getSuccessResponse } from "$lib/models/ServiceResponse";
-
-export async function get() {
-    return {
-        status: 200,
-        body: { 'hej' : "hejda3" }
-    };
-
-}
