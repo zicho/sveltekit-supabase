efa79b406f2f25f084fab200e9da90bc8b0eb3ea
Martin Ström <zicho.calypso@gmail.com>
2021-12-18 22:43:39 +0100

    fixed build error

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,2 @@
-<script lang="ts">
-import { signedInUser } from "$lib/stores/UserStore";
-
-	import { Toasts, addToast } from "as-toast"
-
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-        console.log("mounted")
-        
-	});
-</script>
-
-<h1>Logged in n' shit</h1>
+<h1>You are logged in.</h1>
 
